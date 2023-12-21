b84c9946c1cc55f748d79a692cd9691beae0d324
brenoalmeidaa07 <121868493+brenoalmeidaa07@users.noreply.github.com>
2023-12-21 19:14:07 -0300

    container

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+export default function DashboardPage() {
+    return (
+      <Container 
+      maxWidth="xs"
+     sx={{
+        bgcolor: "blue",
+        color: "white"
+      }}
+      >
+        Hello
+      </Container>
+    );
+    
+      /*<Container>
+        Welcome to MUI Container 
+        This is an example of using the{" "}
+        <code>{"<Container>"}</code>
+        component in Material-UI.
+      </Container>*/
+      
+    
+  }
